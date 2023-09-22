8944a5408b86ad04af321441778faeca5281b54d
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-22 22:02:49 +0530

    finished

---
+++ routes/route.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const Contact = require("../modules/contact");
+const router = express.Router();
+
+router.post("/api/contact", async (req, res) => {
+  try {
+    const { firstName, lastName, email, number, company } = req.body;
+
+    let contact = new Contact({
+      firstName,
+      lastName,
+      email,
+      number,
+      company,
+    });
+
+    user = await contact.save();
+    res.status(200).json({ msg: "Succesfully submitted" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+// Get all contact info
+router.get("/api/contacts", async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
