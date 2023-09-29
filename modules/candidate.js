7dd3254ca8a0dd191db77d8446c0b4ba4c6bdf49
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-29 15:29:21 +0530

    created the candidates api

---
+++ modules/candidate.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const candidatesSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    number: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    portfolio: {
+      type: String,
+      required: true,
+    },
+    cv: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Candidates = mongoose.model("candidates", candidatesSchema);
+module.exports = Candidates;
--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const Contact = require("../modules/contact");
+const Candidates = require("../modules/candidate");
+
 const router = express.Router();
 
 router.post("/api/contact", async (req, res) => {
@@ -21,11 +23,42 @@ router.post("/api/contact", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+router.post("/api/candidate", async (req, res) => {
+  try {
+    const { firstName, lastName, email, number, portfolio, cv, message } =
+      req.body;
+
+    let contact = new Candidates({
+      firstName,
+      lastName,
+      email,
+      number,
+      portfolio,
+      cv,
+      message,
+    });
+
+    user = await contact.save();
+    res.status(200).json({ msg: "Succesfully submitted" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 // Get all contact info
 router.get("/api/contacts", async (req, res) => {
   try {
     const contacts = await Contact.find();
     res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/api/candidates", async (req, res) => {
+  try {
+    const candidates = await Candidates.find();
+    res.status(200).json(candidates);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
